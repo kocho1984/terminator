66a1d05e499d675a0debf41ea9c554efad6a9108
kocho1984 <eblebleo2@gmail.com>
2021-07-06 21:40:47 +0200

    alpha version

--- terminatorlib/exportenv.py
+++ terminatorlib/exportenv.py
@@ -1,8 +1,5 @@
-import os
-import time
 import pyautogui
 from gi.repository import Gtk
-from .util import dbg
 
 cloningMode = False
 
@@ -13,54 +10,21 @@ def setCloningMode(value):
 def getCloningMode():
     return cloningMode
 
-def exportEnvOLD():
-    if not getCloningMode():
-        return
-
-    cmd = '''
-    xdotool key Control_L+z;
-    xdotool type '{declare +x ; declare -x;} | sed -r ';
-    xdotool keydown apostrophe;
-    xdotool keyup apostrophe;
-    xdotool type 's/^/export /';
-    xdotool keydown apostrophe;
-    xdotool keyup apostrophe;
-    xdotool type ' > /tmp/env.txt';
-    xdotool key KP_Enter;
-    xdotool type 'fg';
-    xdotool key KP_Enter;
-    '''
-    os.system(cmd)
-
-    updateGUI()
 
 def exportEnv():
     if not getCloningMode():
         return
-
-    cmd = '''
-    xdotool key Control_L+z;
-    xdotool type '{declare +x ; declare -x;} | sed -r ';
-    xdotool keydown apostrophe;
-    xdotool keyup apostrophe;
-    xdotool type 's/^/export /';
-    xdotool keydown apostrophe;
-    xdotool keyup apostrophe;
-    xdotool type ' > /tmp/env.txt';
-    xdotool key KP_Enter;
-    xdotool type 'fg';
-    xdotool key KP_Enter;
-    '''
     
-    pyautogui.hotkey("command", 'z')
-
-    pyautogui.write("(declare +x | sed -r 's/^/export /' && declare -x) > /tmp/env.txt")
+    pyautogui.hotkey("ctrl", 'z')
+    #pyautogui.write("(declare +x | sed -r 's/^/export /' && declare -x) > /tmp/env.txt")
+    pyautogui.write("(declare +x && declare -x) > /tmp/env.txt")
     pyautogui.press('enter')
 
     pyautogui.write("fg")
     pyautogui.press('enter')
 
     updateGUI()
+
 
 def updateGUI():
     '''Force update of GTK mainloop during a long-running process'''
