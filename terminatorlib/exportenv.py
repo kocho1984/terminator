98378bbdd317c427e4ce5590cc4cd3ede2958ae4
kocho1984 <eblebleo2@gmail.com>
2021-07-06 03:05:54 +0200

    Replaced xdotool with pyautogui

--- terminatorlib/exportenv.py
+++ terminatorlib/exportenv.py
@@ -1,5 +1,6 @@
 import os
 import time
+import pyautogui
 from gi.repository import Gtk
 from .util import dbg
 
@@ -11,6 +12,27 @@ def setCloningMode(value):
 
 def getCloningMode():
     return cloningMode
+
+def exportEnvOLD():
+    if not getCloningMode():
+        return
+
+    cmd = '''
+    xdotool key Control_L+z;
+    xdotool type '{declare +x ; declare -x;} | sed -r ';
+    xdotool keydown apostrophe;
+    xdotool keyup apostrophe;
+    xdotool type 's/^/export /';
+    xdotool keydown apostrophe;
+    xdotool keyup apostrophe;
+    xdotool type ' > /tmp/env.txt';
+    xdotool key KP_Enter;
+    xdotool type 'fg';
+    xdotool key KP_Enter;
+    '''
+    os.system(cmd)
+
+    updateGUI()
 
 def exportEnv():
     if not getCloningMode():
@@ -29,7 +51,14 @@ def exportEnv():
     xdotool type 'fg';
     xdotool key KP_Enter;
     '''
-    os.system(cmd)
+    
+    pyautogui.hotkey("command", 'z')
+
+    pyautogui.write("(declare +x | sed -r 's/^/export /' && declare -x) > /tmp/env.txt")
+    pyautogui.press('enter')
+
+    pyautogui.write("fg")
+    pyautogui.press('enter')
 
     updateGUI()
 
